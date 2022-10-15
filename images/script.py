2d69a6c160c117ae8d80187a47ad6220629b6920
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2022-10-15 12:08:41 +0200

    move mirrored files to the right folder

--- images/script.py
+++ images/script.py
@@ -16,13 +16,13 @@ from PIL import Image
 #     img.save(os.path.join('mirrored_' + os.path.basename(image)))
 
 # creat a .txt for mirroed images based on the .txt of the original images
-for image in glob('resized_and_augmented/*'):
-    with open(os.path.join('resized_and_augmented', os.path.basename(image).split('.')[0] + '.txt')) as f:
-        line = f.readline()
-        with open(os.path.join('resized_and_augmented', "mirrored_" + os.path.basename(image).split('.')[0] + '.txt'), 'w') as f:
-            f.write(line)
+# for image in glob('resized_and_augmented/*'):
+#     with open(os.path.join('resized_and_augmented', os.path.basename(image).split('.')[0] + '.txt')) as f:
+#         line = f.readline()
+#         with open(os.path.join('resized_and_augmented', "mirrored_" + os.path.basename(image).split('.')[0] + '.txt'), 'w') as f:
+#             f.write(line)
 
-#delete all .txt files in resized folder
-# for text in glob('resized_and_augmented/*.txt'):
-#     if "mirrored" in text:
-#         os.remove(text)
+# move all files in current folder to resized_and_augmented folder*
+for image in glob('*'):
+    os.rename(image, os.path.join('resized_and_augmented', image))
+
